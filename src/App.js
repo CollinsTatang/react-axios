54e43e598cf636dc92129abf50d3e9c67a9306aa
Collins Tatang <makungongcollins@gmail.com>
2023-06-16 10:51:45 +0100

    Remove unecesstory files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import './App.css';
-import axios from 'axios';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const client = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/posts" 
